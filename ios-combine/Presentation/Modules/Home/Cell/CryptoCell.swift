c5e1e7ca9dba498f5150937678ba6751e34d97bd
Fernando Salom <fer@rudo.es>
2022-12-31 10:16:16 +0100

    Add cryto cell

---
+++ ios-combine/Presentation/Modules/Home/Cell/CryptoCell.swift
@@ -0,0 +1,35 @@
+//
+//  CryptoCell.swift
+//  coredatasample
+//
+//  Created by Fernando Salom Carratala on 26/11/22.
+//
+
+import UIKit
+
+class CryptoCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+
+    var crypto: CryptoDTO! {
+        didSet {
+            setupUI()
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func setupUI() {
+        nameLabel.text = crypto.name
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
