b11f281f6d1f212d6184b638ab07a6fa3fc2d94a
Fernando Salom <fer@rudo.es>
2023-01-01 14:53:42 +0100

    Add tableview

--- ios-combine/Presentation/Modules/Home/HomeViewController.swift
+++ ios-combine/Presentation/Modules/Home/HomeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Combine
 
 final class HomeViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
     private var cancellables = Set<AnyCancellable>()
     var viewModel: HomeViewModelProtocol!
 
@@ -24,6 +25,7 @@ final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         viewModel.viewDidLoad()
         setupBindings()
+        configure()
         super.viewDidLoad()
     }
 
@@ -41,11 +43,32 @@ final class HomeViewController: UIViewController {
                     .sink(receiveValue: { (response) in
                         print("COMBINE")
                         print(response)
+                        self.tableView.reloadData()
                     })
                     .store(in: &cancellables)
     }
+
+    func configure() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        self.tableView.register(UINib(nibName: "CryptoCell", bundle: nil),
+                                forCellReuseIdentifier: "CryptoCell")
+        self.tableView.rowHeight = 60
+    }
 }
 
-struct Response: Codable {
-    let message: String
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.cryptos.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell", for: indexPath) as! CryptoCell
+        cell.crypto = viewModel.cryptos[indexPath.row]
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        60
+    }
 }
